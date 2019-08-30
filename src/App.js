ce40043a09878b59d2265d930263dff52b2329d1
Susan Chen <30938981+susanschen@users.noreply.github.com>
2019-08-30 18:44:10 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>To Do</h1>
+    </div>
+  );
+}
